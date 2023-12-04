7f7d28d761df52cdae3a0ef6eccff1a26ec3fb08
Nick Le <ngoc.letuan@rewardstyle.com>
2023-12-04 23:51:43 +0200

    Day3 correct part1

--- MyPlayground.playground/Sources/Day3.swift
+++ MyPlayground.playground/Sources/Day3.swift
@@ -1,11 +1,17 @@
 import Foundation
 
+struct ValidPart {
+    let number: String
+    let i: Int
+    let j: Int
+}
+
 public class Day3 {
     private let fileInput: String
     
     private(set) var matrix = [[String]]()
-    private var numbers = [String: (Int, Int)]()
-    
+    private var numbers = [ValidPart]()
+
     public init(
         fileInput: String = "day3"
     ) {
@@ -21,7 +27,7 @@ public class Day3 {
                 var row = [String]()
                 var number = ""
                 for (j, c) in line.enumerated() {
-                    // add the character to the row of metrix
+                    // add the character to the row of matrix
                     row.append(String(c))
                     
                     // at the same time we collect the number from the matrix
@@ -29,71 +35,103 @@ public class Day3 {
                         number += String(c)
                     } else {
                         if !number.isEmpty {
-                            numbers[number] = (i, j)
+                            numbers.append(ValidPart(number: number, i: i, j: j - number.count))
                         }
                         number = ""
                     }
                 }
+                // the number is in the end of line
+                if !number.isEmpty {
+                    numbers.append(ValidPart(number: number, i: i, j: line.count - number.count))
+                }
                 matrix.append(row)
             }
             
-            for (number, index) in numbers {
-                if !verifyNumber(number, i: index.0, j: index.1) {
-                    print("Part: \(number)")
+            var sum = 0
+            for number in numbers {
+                if verifyNumber(number.number, i: number.i, j: number.j) {
+                    print(number)
+                    sum += (Int(number.number) ?? 0)
                 }
             }
             
-            var sum = 0
-//            print("The answer is: \(sum)")
+            print("The answer is: \(sum)")
         } catch {
             print(error)
         }
     }
     
-    private func verifyNumber(_ number: String, i: Int, j: Int) -> Bool {
+    public func verifyNumber(_ number: String, i: Int, j: Int) -> Bool {
         let rowBefore = i - 1
         let rowAfter = i + 1
         let columnBefore = j - 1
-        let columnAfter = j + 1
-        
-        let fromRow = max(rowBefore, 0)
-        let toRow = min(rowAfter, matrix[0].count - 1)
-        
-        let fromColumn = max(columnBefore, 0)
-        let toColumn = min(columnAfter, matrix[0].count - 1)
-        
+        let columnAfter = j + number.count
+
+//        print("rowBefore \(rowBefore)")
+//        print("rowAfter \(rowAfter)")
+//        print("columnBefore \(columnBefore)")
+//        print("columnAfter \(columnAfter)")
+
         if rowBefore >= 0 {
-            for k in fromColumn...toColumn {
-                if matrix[rowBefore][k] != "." {
+            for k in columnBefore...columnAfter {
+                if 
+                    k >= 0,
+                    k <= matrix[rowBefore].count - 1,
+                    matrix[rowBefore][k] != "."
+                {
+                    print("1")
+                    print("value: \(matrix[rowBefore][k])")
                     return true
                 }
             }
         }
         
         if rowAfter < matrix.count {
-            for k in fromColumn...toColumn {
-                if matrix[rowAfter][k] != "." {
+            for k in columnBefore...columnAfter {
+                if 
+                    k >= 0,
+                    k <= matrix[rowAfter].count - 1,
+                    matrix[rowAfter][k] != "."
+                {
+                    print("2")
+                    print("value: \(matrix[rowAfter][k])")
                     return true
                 }
             }
         }
         
         if columnBefore >= 0 {
-            for k in fromRow...toRow {
-                if matrix[k][columnBefore] != "." {
+            for k in rowBefore...rowAfter {
+                if
+                    k >= 0,
+                    k <= matrix.count - 1,
+                    matrix[k][columnBefore] != "."
+                {
+                    print("3")
+                    print("value: \(matrix[k][columnBefore])")
                     return true
                 }
             }
         }
         
         if columnAfter < matrix.count {
-            for k in fromRow...toRow {
-                if matrix[k][columnAfter] != "." {
+            for k in rowBefore...rowAfter {
+                if
+                    k >= 0,
+                    k <= matrix.count - 1,
+                    matrix[k][columnAfter] != "."
+                {
+                    print("4")
+                    print("value: \(matrix[k][columnAfter])")
+//                    print("k: \(k)")
+//                    print("fromRow: \(rowBefore)")
+//                    print("toRow: \(rowAfter)")
+//                    print("columnAfter: \(columnAfter)")
                     return true
                 }
             }
         }
-        
-        return true
+
+        return false
     }
 }
